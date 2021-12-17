e496fe16784b62e48b1ee51d9c3927bc42735b36
π12Qα <41827134+firqaaa@users.noreply.github.com>
2021-12-17 13:29:12 +0700

    Create example_subdag_operator.py

---
+++ dags/example_subdag_operator.py
@@ -0,0 +1,48 @@
+import airflow
+from airflow.models import DAG
+from airflow.example_dags.subdags.subdag import subdag
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.subdag_operator import SubDagOperator
+
+DAG_NAME = 'example_subdag_operator'
+
+args = {
+    'owner': 'airflow',
+    'start_date': airflow.utils.dates.days_ago(2)
+}
+
+dag = DAG(
+    dag_id=DAG_NAME,
+    default_args=args,
+    schedule_interval='@once'
+)
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag
+)
+
+section_1 = SubDagOperator(
+    task_id='section-1',
+    subdag=subdag(DAG_NAME, 'section-1', args),
+    dag=dag
+)
+
+some_other_task = DummyOperator(
+    task_id='some-other-task',
+    dag=dag
+)
+
+section_2 = SubDagOperator(
+    task_id='section-2',
+    subdag=subdag(DAG_NAME, 'section-2', args),
+    dag=dag
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag
+)
+
+start >> section_1 >> some_other_task >> section_2 >> end
+
